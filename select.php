8f7a45f9e38c9db51aff699dcdf78adac8803658
Denton-x <luckycyusa3@gmail.com>
2026-03-30 08:33:14 -0700

    Add files via upload

---
+++ select.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script>
+    setInterval(function() {
+        location.reload();
+    }, 1021000); // refresh every 5 seconds
+</script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DASHBOARD</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+$host="localhost";
+$username="root";
+$pass="";
+$db="project_db";
+$con=mysqli_connect("localhost","root","","project_db");
+$select=mysqli_query($con,"select * from sensor_data");
+if(mysqli_num_rows($select)>0){
+    ?>
+    <p><h1> <u>DHTT DASHBOARD</u></h1></p>
+    <table border="1">
+    <tr><td>ID</td><td>temperature</td><td>humidity</td><td>created_at</td></tr>
+    <?php
+    while($data=mysqli_fetch_array($select)){
+        ?>
+      <tr><td><?php echo $data["id"];?></td><td><?php echo $data["temperature"];?></td><td><?php echo $data["humidity"];?></td><td><?php echo $data["created_at"];?></td></tr>
+
+      <?php
+
+    }
+    ?>
+    </table>
+    <?php
+
+}
